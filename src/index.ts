4b7c2726b8a401a4d55042308fff74f3aa4c34e0
Martin Mitrevski <martinmitrevski.oh@gmail.com>
2025-11-11 11:01:02 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export { Agent, type AgentOptions, type RegisterToolOptions } from './Agent';
+export { createDefaultTools } from './defaultTools';
+export { AgentPlatform } from './types';
+export type { AIAgent } from './types';
+export {
+  VercelAIAgent,
+  type AgentTool,
+  type ClientToolDefinition,
+  type ToolExecutionContext,
+  type Mem0ContextInput,
+} from './VercelAIAgent';
